7d04f9f30a155e5bf5d202b52897127a5bbdfacb
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-02 09:56:22 +0900

    refactor: Use absolute import

--- deepfold/utils/geometry/__init__.py
+++ deepfold/utils/geometry/__init__.py
@@ -1,7 +1,15 @@
 # DeepFold Team
 
 
-from .rigid import Rigid, Rotation
-from .rigid_matrix_vector import Rigid3Array
-from .rotation_matrix import Rot3Array
-from .vector import Vec3Array
+from deepfold.utils.geometry.rigid import Rigid, Rotation
+from deepfold.utils.geometry.rigid_matrix_vector import Rigid3Array
+from deepfold.utils.geometry.rotation_matrix import Rot3Array
+from deepfold.utils.geometry.vector import Vec3Array
+
+__all__ = [
+    "Rigid",
+    "Rotation",
+    "Rigid3Array",
+    "Rot3Array",
+    "Vec3Array",
+]
