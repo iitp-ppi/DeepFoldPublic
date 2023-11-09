3c840cd8607fd6c33adb39f019d3f0e592e3dc8d
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-09 11:25:27 +0900

    feat: Routines to generate input features

--- deepfold/model/alphafold/pipeline/make_feats.py
+++ deepfold/model/alphafold/pipeline/make_feats.py
@@ -1,4 +1,5 @@
 # Copyright 2023 DeepFold Team
+# Copyright 2021 AlQuraishi Laboratory
 # Copyright 2021 DeepMind Technologies Limited
 
 import logging
@@ -15,69 +16,96 @@ logger = logging.getLogger(__name__)
 FeatureDict = MutableMapping[str, np.ndarray]
 
 
-def make_sequence_feature(
-    sequence: str,
-    description: str,
-    num_res: int,
+def empty_template_feats(n_res: int) -> FeatureDict:
+    return {
+        "template_aatype": np.zeros((0, n_res)).astype(np.int64),
+        "template_all_atom_positions": np.zeros((0, n_res, 37, 3)).astype(np.float32),
+        "template_sum_probs": np.zeros((0, 1)).astype(np.float32),
+        "template_all_atom_mask": np.zeros((0, n_res, 37)).astype(np.float32),
+    }
+
+
+def make_template_features(
+    input_sequence: str,
+    hits: Sequence[Any],
+    template_featurizer: Any,
+    query_pdb_code: Optional[str] = None,
+    query_release_date: Optional[str] = None,
 ) -> FeatureDict:
-    """
-    Construct a feature dict of sequence features.
-    """
-    features = {}
+    hits_cat = sum(hits.values(), [])
+    if len(hits_cat) == 0 or template_featurizer is None:
+        template_features = empty_template_feats(len(input_sequence))
+    else:
+        templates_result = template_featurizer.get_templates(
+            query_sequence=input_sequence,
+            query_pdb_code=query_pdb_code,
+            query_release_date=query_release_date,
+            hits=hits_cat,
+        )
+        template_features = templates_result.features
 
-    features["aatype"] = rc.sequence_to_onehot(
-        sequence=sequence,
-        mapping=rc.restype_order_with_x,
-        map_unknown_to_x=True,
-    )
-    features["between_segment_residues"] = np.zeros((num_res,), dtype=np.int32)
-    features["domain_name"] = np.array([description.encode()], dtype=np.object_)
-    features["residue_index"] = np.array(range(num_res), dtype=np.int32)  # Start from zero
-    features["seq_length"] = np.array([num_res] * num_res, dtype=np.int32)
-    features["sequence"] = np.array([sequence.encode()], dtype=np.object_)
+        # TODO: The template featurizer doesn't format empty template features properly.
+        if template_features["template_aatype"].shape[0] == 0:
+            template_features = empty_template_feats(len(input_sequence))
 
-    return features
-
-
-def make_msa_features(msas: Sequence[parsers.MSA]) -> FeatureDict:
-    """
-    Construct a feature dict of MSA features.
-    """
-    if not msas:
-        raise ValueError("At least one MSA must be provided")
-
-    int_msa = []
-    deletion_matrix = []
-    species_ids = []
-    seen_sequences = set()
-
-    for msa_index, msa in enumerate(msas):
-        if not msa:
-            raise ValueError(f"MSA {msa_index} must contain at least one sequence")
-        for seq_index, seq in enumerate(msa.sequences):
-            if seq in seen_sequences:
-                continue
-            seen_sequences.add(seq)
-            int_msa.append([rc.HHBLITS_AA_TO_ID[res] for res in seq])
-            deletion_matrix.append(msa.deletion_matrix[seq_index])
-            identifiers = msa_identifiers.get_identifiers(msa.descriptions[seq_index])
-            species_ids.append(identifiers.species_id.encode())
-
-    num_res = len(msas[0].sequences[0])
-    num_alignments = len(int_msa)
-
-    features = {}
-
-    features["deletion_matrix_int"] = np.array(deletion_matrix, dtype=np.int32)
-    features["msa"] = np.array(int_msa, dtype=np.int32)
-    features["num_alignments"] = np.array([num_alignments] * num_res, dtype=np.int32)
-    features["msa_species_identifiers"] = np.array(species_ids, dtype=np.object_)
-
-    return features
+    return template_features
 
 
-# TODO: process_templates
+def unify_template_features(template_feature_list: Sequence[FeatureDict]) -> FeatureDict:
+    out_dicts = []
+    seq_lens = [fd["template_aatype"].shape[1] for fd in template_feature_list]
+    for i, fd in enumerate(template_feature_list):
+        out_dict = {}
+        n_templates, n_res = fd["template_aatype"].shape[:2]
+        for k, v in fd.items():
+            seq_keys = [
+                "template_aatype",
+                "template_all_atom_positions",
+                "template_all_atom_mask",
+            ]
+            if k in seq_keys:
+                new_shape = list(v.shape)
+                assert new_shape[1] == n_res
+                new_shape[1] = sum(seq_lens)
+                new_array = np.zeros(new_shape, dtype=v.dtype)
 
-# TODO: run_msa_tool
+                if k == "template_aatype":
+                    new_array[..., rc.HHBLITS_AA_TO_ID["-"]] = 1
 
-# TODO: AlphaFoldPipeline
+                offset = sum(seq_lens[:i])
+                new_array[:, offset : offset + seq_lens[i]] = v
+                out_dict[k] = new_array
+            else:
+                out_dict[k] = v
+
+        chain_indices = np.array(n_templates * [i])
+        out_dict["template_chain_index"] = chain_indices
+
+        if n_templates != 0:
+            out_dicts.append(out_dict)
+
+    if len(out_dicts) > 0:
+        out_dict = {k: np.concatenate([od[k] for od in out_dicts]) for k in out_dicts[0]}
+    else:
+        out_dict = empty_template_feats(sum(seq_lens))
+
+    return out_dict
+
+
+# TODO: make_sequence_features
+
+# TODO: make_mmcif_features
+
+# TODO: make_protein_featuers
+
+# TODO: make_pdb_features
+
+# TODO: make_msa_features
+
+# TODO: make_dummy_msa_features
+
+# TODO: make_sequence_features_with_custom_template
+
+# TODO: AlignmentRunner
+
+# TODO: DataPipeline
