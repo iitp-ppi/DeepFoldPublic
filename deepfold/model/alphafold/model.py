8d385ac08b3e62a759e4012c18ceda83d9e7ef36
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-09 14:35:58 +0900

    docs: Update license

---
+++ deepfold/model/alphafold/model.py
@@ -0,0 +1,3 @@
+# Copyright 2023 DeepFold Team
+# Copyright 2021 AlQuraishi Laboratory
+# Copyright 2021 DeepMind Technologies Limited
--- deepfold/model/alphafold/pipeline/monomer_pipeline.py
+++ deepfold/model/alphafold/pipeline/monomer_pipeline.py
@@ -1,3 +1,8 @@
 # Copyright 2023 DeepFold Team
 # Copyright 2021 AlQuraishi Laboratory
 # Copyright 2021 DeepMind Technologies Limited
+
+
+import logging
+
+logger = logging.getLogger(__name__)
