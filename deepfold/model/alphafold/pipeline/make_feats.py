9bb4f53ddcd41d19fe0815ca077d2eed28c152ff
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-09 13:45:07 +0900

    feat: Feature generation routines

--- deepfold/model/alphafold/pipeline/make_feats.py
+++ deepfold/model/alphafold/pipeline/make_feats.py
@@ -92,20 +92,80 @@ def unify_template_features(template_feature_list: Sequence[FeatureDict]) -> Fea
     return out_dict
 
 
-# TODO: make_sequence_features
+def make_sequence_features(sequence: str, description: str, num_res: int) -> FeatureDict:
+    """Construct a feature dict of sequence features."""
+    features = {}
+    features["aatype"] = rc.sequence_to_onehot(
+        sequence=sequence,
+        mapping=rc.restype_order_with_x,
+        map_unknown_to_x=True,
+    )
+    features["between_segment_residues"] = np.zeros((num_res,), dtype=np.int32)
+    features["domain_name"] = np.array([description.encode("utf-8")], dtype=np.object_)
+    features["residue_index"] = np.array(range(num_res), dtype=np.int32)
+    features["seq_length"] = np.array([num_res] * num_res, dtype=np.int32)
+    features["sequence"] = np.array([sequence.encode("utf-8")], dtype=np.object_)
+    return features
+
 
 # TODO: make_mmcif_features
+
+
+def _aatype_to_str_sequence(aatype):
+    return "".join([rc.restypes_with_x[aatype[i]] for i in range(len(aatype))])
+
 
 # TODO: make_protein_featuers
 
 # TODO: make_pdb_features
 
-# TODO: make_msa_features
 
-# TODO: make_dummy_msa_features
+def make_msa_features(
+    msas: Sequence[Sequence[str]],
+    deletion_matrices: Sequence[parsers.DeletionMatrix],
+) -> FeatureDict:
+    """Constructs a feature dict of MSA features."""
+    if not msas:
+        raise ValueError("At least one MSA must be provided.")
+
+    int_msa = []
+    deletion_matrix = []
+    seen_sequences = set()
+    for msa_index, msa in enumerate(msas):
+        if not msa:
+            raise ValueError(f"MSA {msa_index} must contain at least one sequence.")
+        for sequence_index, sequence in enumerate(msa):
+            if sequence in seen_sequences:
+                continue
+            seen_sequences.add(sequence)
+            int_msa.append([rc.HHBLITS_AA_TO_ID[res] for res in sequence])
+            deletion_matrix.append(deletion_matrices[msa_index][sequence_index])
+
+    num_res = len(msas[0][0])
+    num_alignments = len(int_msa)
+    features = {}
+    features["deletion_matrix_int"] = np.array(deletion_matrix, dtype=np.int32)
+    features["msa"] = np.array(int_msa, dtype=np.int32)
+    features["num_alignments"] = np.array([num_alignments] * num_res, dtype=np.int32)
+    return features
+
+
+# Generate 1-sequence MSA features having only the input sequence
+def make_dummy_msa_feats(input_sequence):
+    msas = [[input_sequence]]
+    deletion_matrices = [[[0 for _ in input_sequence]]]
+    msa_features = make_msa_features(
+        msas=msas,
+        deletion_matrices=deletion_matrices,
+    )
+
+    return msa_features
+
 
 # TODO: make_sequence_features_with_custom_template
 
+
 # TODO: AlignmentRunner
+
 
 # TODO: DataPipeline
