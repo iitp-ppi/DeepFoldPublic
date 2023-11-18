4fb2691f5e702614db41a54362a2aec4752429d6
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-18 14:30:20 +0900

    style: Reformat

--- deepfold/model/alphafold/utils/feats.py
+++ deepfold/model/alphafold/utils/feats.py
@@ -66,9 +66,7 @@ def atom14_to_atom37(atom14_data: torch.Tensor, batch: FeatureDict) -> torch.Ten
         atom14_data,
         batch["residx_atom37_to_atom14"],
         dim=-2,
-        num_batch_dims=len(
-            atom14_data.shape[:-2],
-        ),
+        num_batch_dims=len(atom14_data.shape[:-2]),
     )
     atom37_data = atom37_data * batch["atom37_atom_exists"][..., None]
 
