c95d30e5298072ea69c210bc5b0972010d3aaa36
Minsoo Kim <vv137.xyz@gmail.com>
2023-11-18 14:30:43 +0900

    feat: Partial import

--- deepfold/model/alphafold/utils/import_weights.py
+++ deepfold/model/alphafold/utils/import_weights.py
@@ -12,6 +12,7 @@ from typing import List, Union
 import numpy as np
 import torch
 
+from deepfold.model.alphafold.model import AlphaFold
 from deepfold.model.alphafold.nn.evoformer import EvoformerBlock
 
 logger = logging.getLogger(__name__)
@@ -87,6 +88,7 @@ def stacked(param_dict_list, out=None):
 
 
 def assign(translation_dict, orig_weights):
+    logger.debug("Assign JAX parameters to the model")
     for k, param in translation_dict.items():
         with torch.no_grad():
             weights = torch.as_tensor(orig_weights[k])
@@ -102,211 +104,214 @@ def assign(translation_dict, orig_weights):
                 for p, w in zip(ref, weights):
                     p.copy_(w)
             except:
-                logger.debug(f"{k}: {ref[0].shape} vs {weights[0].shape}")
+                logger.debug(f"{k}: {tuple(ref[0].shape)} != {tuple(weights[0].shape)}")
                 raise
 
 
+#######################
+# Primitive templates
+#######################
+
+# Linear layer
+
+LinearWeight = lambda l: (Param(l, param_type=ParamType.LinearWeight))
+
+LinearBias = lambda l: (Param(l))
+
+# Multi-Head Attention
+
+LinearWeightMHA = lambda l: (Param(l, param_type=ParamType.LinearWeightMHA))
+
+LinearBiasMHA = lambda b: (Param(b, param_type=ParamType.LinearBiasMHA))
+
+# Outer Product Mean
+
+LinearWeightOPM = lambda l: (Param(l, param_type=ParamType.LinearWeightOPM))
+
+#######################
+# Some templates
+#######################
+
+LinearParams = lambda l: {
+    "weights": LinearWeight(l.weight),
+    "bias": LinearBias(l.bias),
+}
+
+LayerNormParams = lambda l: {
+    "scale": Param(l.weight),
+    "offset": Param(l.bias),
+}
+
+AttentionParams = lambda att: {
+    "query_w": LinearWeightMHA(att.linear_q.weight),
+    "key_w": LinearWeightMHA(att.linear_k.weight),
+    "value_w": LinearWeightMHA(att.linear_v.weight),
+    "output_w": Param(
+        att.linear_o.weight,
+        param_type=ParamType.LinearMHAOutputWeight,
+    ),
+    "output_b": LinearBias(att.linear_o.bias),
+}
+
+AttentionGatedParams = lambda att: dict(
+    **AttentionParams(att),
+    **{
+        "gating_w": LinearWeightMHA(att.linear_g.weight),
+        "gating_b": LinearBiasMHA(att.linear_g.bias),
+    },
+)
+
+GlobalAttentionParams = lambda att: dict(
+    AttentionGatedParams(att),
+    key_w=LinearWeight(att.linear_k.weight),
+    value_w=LinearWeight(att.linear_v.weight),
+)
+
+TriAttParams = lambda tri_att: {
+    "query_norm": LayerNormParams(tri_att.layer_norm),
+    "feat_2d_weights": LinearWeight(tri_att.linear.weight),
+    "attention": AttentionGatedParams(tri_att.mha),
+}
+
+TriMulOutParams = lambda tri_mul: {
+    "layer_norm_input": LayerNormParams(tri_mul.layer_norm_in),
+    "left_projection": LinearParams(tri_mul.linear_a_p),
+    "right_projection": LinearParams(tri_mul.linear_b_p),
+    "left_gate": LinearParams(tri_mul.linear_a_g),
+    "right_gate": LinearParams(tri_mul.linear_b_g),
+    "center_layer_norm": LayerNormParams(tri_mul.layer_norm_out),
+    "output_projection": LinearParams(tri_mul.linear_z),
+    "gating_linear": LinearParams(tri_mul.linear_g),
+}
+
+# see commit b88f8da on the Alphafold repo
+# Alphafold swaps the pseudocode's a and b between the incoming/outcoming
+# iterations of triangle multiplication, which is confusing and not
+# reproduced in our implementation.
+TriMulInParams = lambda tri_mul: {
+    "layer_norm_input": LayerNormParams(tri_mul.layer_norm_in),
+    "left_projection": LinearParams(tri_mul.linear_b_p),
+    "right_projection": LinearParams(tri_mul.linear_a_p),
+    "left_gate": LinearParams(tri_mul.linear_b_g),
+    "right_gate": LinearParams(tri_mul.linear_a_g),
+    "center_layer_norm": LayerNormParams(tri_mul.layer_norm_out),
+    "output_projection": LinearParams(tri_mul.linear_z),
+    "gating_linear": LinearParams(tri_mul.linear_g),
+}
+
+PairTransitionParams = lambda pt: {
+    "input_layer_norm": LayerNormParams(pt.layer_norm),
+    "transition1": LinearParams(pt.linear_1),
+    "transition2": LinearParams(pt.linear_2),
+}
+
+MSAAttParams = lambda matt: {
+    "query_norm": LayerNormParams(matt.layer_norm_m),
+    "attention": AttentionGatedParams(matt.mha),
+}
+
+MSAColAttParams = lambda matt: {
+    "query_norm": LayerNormParams(matt._msa_att.layer_norm_m),
+    "attention": AttentionGatedParams(matt._msa_att.mha),
+}
+
+MSAGlobalAttParams = lambda matt: {
+    "query_norm": LayerNormParams(matt.layer_norm_m),
+    "attention": GlobalAttentionParams(matt.global_attention),
+}
+
+MSAAttPairBiasParams = lambda matt: dict(
+    **MSAAttParams(matt),
+    **{
+        "feat_2d_norm": LayerNormParams(matt.layer_norm_z),
+        "feat_2d_weights": LinearWeight(matt.linear_z.weight),
+    },
+)
+
+IPAParams = lambda ipa: {
+    "q_scalar": LinearParams(ipa.linear_q),
+    "kv_scalar": LinearParams(ipa.linear_kv),
+    "q_point_local": LinearParams(ipa.linear_q_points),
+    "kv_point_local": LinearParams(ipa.linear_kv_points),
+    "trainable_point_weights": Param(param=ipa.head_weights, param_type=ParamType.Other),
+    "attention_2d": LinearParams(ipa.linear_b),
+    "output_projection": LinearParams(ipa.linear_out),
+}
+
+TemplatePairBlockParams = lambda b: {
+    "triangle_attention_starting_node": TriAttParams(b.tri_att_start),
+    "triangle_attention_ending_node": TriAttParams(b.tri_att_end),
+    "triangle_multiplication_outgoing": TriMulOutParams(b.tri_mul_out),
+    "triangle_multiplication_incoming": TriMulInParams(b.tri_mul_in),
+    "pair_transition": PairTransitionParams(b.pair_transition),
+}
+
+MSATransitionParams = lambda m: {
+    "input_layer_norm": LayerNormParams(m.layer_norm),
+    "transition1": LinearParams(m.linear_1),
+    "transition2": LinearParams(m.linear_2),
+}
+
+OuterProductMeanParams = lambda o: {
+    "layer_norm_input": LayerNormParams(o.layer_norm),
+    "left_projection": LinearParams(o.linear_1),
+    "right_projection": LinearParams(o.linear_2),
+    "output_w": LinearWeightOPM(o.linear_out.weight),
+    "output_b": LinearBias(o.linear_out.bias),
+}
+
+
+def EvoformerBlockParams(b: EvoformerBlock, is_extra_msa=False):
+    if is_extra_msa:
+        col_att_name = "msa_column_global_attention"
+        msa_col_att_params = MSAGlobalAttParams(b.msa.msa_att_col)
+    else:
+        col_att_name = "msa_column_attention"
+        msa_col_att_params = MSAColAttParams(b.msa.msa_att_col)
+
+    d = {
+        "msa_row_attention_with_pair_bias": MSAAttPairBiasParams(b.msa.msa_att_row),
+        col_att_name: msa_col_att_params,
+        "msa_transition": MSATransitionParams(b.msa.msa_transition),
+        "outer_product_mean": OuterProductMeanParams(b.communication),
+        "triangle_multiplication_outgoing": TriMulOutParams(b.pair.tri_mul_out),
+        "triangle_multiplication_incoming": TriMulInParams(b.pair.tri_mul_in),
+        "triangle_attention_starting_node": TriAttParams(b.pair.tri_att_start),
+        "triangle_attention_ending_node": TriAttParams(b.pair.tri_att_end),
+        "pair_transition": PairTransitionParams(b.pair.pair_transition),
+    }
+
+    return d
+
+
+ExtraMSABlockParams = partial(EvoformerBlockParams, is_extra_msa=True)
+
+FoldIterationParams = lambda sm: {
+    "invariant_point_attention": IPAParams(sm.ipa),
+    "attention_layer_norm": LayerNormParams(sm.layer_norm_ipa),
+    "transition": LinearParams(sm.transition.layers[0].linear_1),
+    "transition_1": LinearParams(sm.transition.layers[0].linear_2),
+    "transition_2": LinearParams(sm.transition.layers[0].linear_3),
+    "transition_layer_norm": LayerNormParams(sm.transition.layer_norm),
+    "affine_update": LinearParams(sm.bb_update.linear),
+    "rigid_sidechain": {
+        "input_projection": LinearParams(sm.angle_resnet.linear_in),
+        "input_projection_1": LinearParams(sm.angle_resnet.linear_initial),
+        "resblock1": LinearParams(sm.angle_resnet.layers[0].linear_1),
+        "resblock2": LinearParams(sm.angle_resnet.layers[0].linear_2),
+        "resblock1_1": LinearParams(sm.angle_resnet.layers[1].linear_1),
+        "resblock2_1": LinearParams(sm.angle_resnet.layers[1].linear_2),
+        "unnormalized_angles": LinearParams(sm.angle_resnet.linear_out),
+    },
+}
+
+
 def generate_translation_dict(
-    model: torch.nn.Module,
+    model: AlphaFold,
     *,
     enable_template: bool = True,
     enable_ptm: bool = False,
 ):
-    #######################
-    # Primitive templates
-    #######################
-
-    # Linear layer
-
-    LinearWeight = lambda l: (Param(l, param_type=ParamType.LinearWeight))
-
-    LinearBias = lambda l: (Param(l))
-
-    # Multi-Head Attention
-
-    LinearWeightMHA = lambda l: (Param(l, param_type=ParamType.LinearWeightMHA))
-
-    LinearBiasMHA = lambda b: (Param(b, param_type=ParamType.LinearBiasMHA))
-
-    # Outer Product Mean
-
-    LinearWeightOPM = lambda l: (Param(l, param_type=ParamType.LinearWeightOPM))
-
-    #######################
-    # Some templates
-    #######################
-
-    LinearParams = lambda l: {
-        "weights": LinearWeight(l.weight),
-        "bias": LinearBias(l.bias),
-    }
-
-    LayerNormParams = lambda l: {
-        "scale": Param(l.weight),
-        "offset": Param(l.bias),
-    }
-
-    AttentionParams = lambda att: {
-        "query_w": LinearWeightMHA(att.linear_q.weight),
-        "key_w": LinearWeightMHA(att.linear_k.weight),
-        "value_w": LinearWeightMHA(att.linear_v.weight),
-        "output_w": Param(
-            att.linear_o.weight,
-            param_type=ParamType.LinearMHAOutputWeight,
-        ),
-        "output_b": LinearBias(att.linear_o.bias),
-    }
-
-    AttentionGatedParams = lambda att: dict(
-        **AttentionParams(att),
-        **{
-            "gating_w": LinearWeightMHA(att.linear_g.weight),
-            "gating_b": LinearBiasMHA(att.linear_g.bias),
-        },
-    )
-
-    GlobalAttentionParams = lambda att: dict(
-        AttentionGatedParams(att),
-        key_w=LinearWeight(att.linear_k.weight),
-        value_w=LinearWeight(att.linear_v.weight),
-    )
-
-    TriAttParams = lambda tri_att: {
-        "query_norm": LayerNormParams(tri_att.layer_norm),
-        "feat_2d_weights": LinearWeight(tri_att.linear.weight),
-        "attention": AttentionGatedParams(tri_att.mha),
-    }
-
-    TriMulOutParams = lambda tri_mul: {
-        "layer_norm_input": LayerNormParams(tri_mul.layer_norm_in),
-        "left_projection": LinearParams(tri_mul.linear_a_p),
-        "right_projection": LinearParams(tri_mul.linear_b_p),
-        "left_gate": LinearParams(tri_mul.linear_a_g),
-        "right_gate": LinearParams(tri_mul.linear_b_g),
-        "center_layer_norm": LayerNormParams(tri_mul.layer_norm_out),
-        "output_projection": LinearParams(tri_mul.linear_z),
-        "gating_linear": LinearParams(tri_mul.linear_g),
-    }
-
-    # see commit b88f8da on the Alphafold repo
-    # Alphafold swaps the pseudocode's a and b between the incoming/outcoming
-    # iterations of triangle multiplication, which is confusing and not
-    # reproduced in our implementation.
-    TriMulInParams = lambda tri_mul: {
-        "layer_norm_input": LayerNormParams(tri_mul.layer_norm_in),
-        "left_projection": LinearParams(tri_mul.linear_b_p),
-        "right_projection": LinearParams(tri_mul.linear_a_p),
-        "left_gate": LinearParams(tri_mul.linear_b_g),
-        "right_gate": LinearParams(tri_mul.linear_a_g),
-        "center_layer_norm": LayerNormParams(tri_mul.layer_norm_out),
-        "output_projection": LinearParams(tri_mul.linear_z),
-        "gating_linear": LinearParams(tri_mul.linear_g),
-    }
-
-    PairTransitionParams = lambda pt: {
-        "input_layer_norm": LayerNormParams(pt.layer_norm),
-        "transition1": LinearParams(pt.linear_1),
-        "transition2": LinearParams(pt.linear_2),
-    }
-
-    MSAAttParams = lambda matt: {
-        "query_norm": LayerNormParams(matt.layer_norm_m),
-        "attention": AttentionGatedParams(matt.mha),
-    }
-
-    MSAColAttParams = lambda matt: {
-        "query_norm": LayerNormParams(matt.layer_norm_m),
-        "attention": AttentionGatedParams(matt._msa_att),
-    }
-
-    MSAGlobalAttParams = lambda matt: {
-        "query_norm": LayerNormParams(matt.layer_norm_m),
-        "attention": GlobalAttentionParams(matt.global_attention),
-    }
-
-    MSAAttPairBiasParams = lambda matt: dict(
-        **MSAAttParams(matt),
-        **{
-            "feat_2d_norm": LayerNormParams(matt.layer_norm_z),
-            "feat_2d_weights": LinearWeight(matt.linear_z.weight),
-        },
-    )
-
-    IPAParams = lambda ipa: {
-        "q_scalar": LinearParams(ipa.linear_q),
-        "kv_scalar": LinearParams(ipa.linear_kv),
-        "q_point_local": LinearParams(ipa.linear_q_points),
-        "kv_point_local": LinearParams(ipa.linear_kv_points),
-        "trainable_point_weights": Param(param=ipa.head_weights, param_type=ParamType.Other),
-        "attention_2d": LinearParams(ipa.linear_b),
-        "output_projection": LinearParams(ipa.linear_out),
-    }
-
-    TemplatePairBlockParams = lambda b: {
-        "triangle_attention_starting_node": TriAttParams(b.tri_att_start),
-        "triangle_attention_ending_node": TriAttParams(b.tri_att_end),
-        "triangle_multiplication_outgoing": TriMulOutParams(b.tri_mul_out),
-        "triangle_multiplication_incoming": TriMulInParams(b.tri_mul_in),
-        "pair_transition": PairTransitionParams(b.pair_transition),
-    }
-
-    MSATransitionParams = lambda m: {
-        "input_layer_norm": LayerNormParams(m.layer_norm),
-        "transition1": LinearParams(m.linear_1),
-        "transition2": LinearParams(m.linear_2),
-    }
-
-    OuterProductMeanParams = lambda o: {
-        "layer_norm_input": LayerNormParams(o.layer_norm),
-        "left_projection": LinearParams(o.linear_1),
-        "right_projection": LinearParams(o.linear_2),
-        "output_w": LinearWeightOPM(o.linear_out.weight),
-        "output_b": LinearBias(o.linear_out.bias),
-    }
-
-    def EvoformerBlockParams(b: EvoformerBlock, is_extra_msa=False):
-        if is_extra_msa:
-            col_att_name = "msa_column_global_attention"
-            msa_col_att_params = MSAGlobalAttParams(b.msa.msa_att_col)
-        else:
-            col_att_name = "msa_column_attention"
-            msa_col_att_params = MSAColAttParams(b.msa.msa_att_col)
-
-        d = {
-            "msa_row_attention_with_pair_bias": MSAAttPairBiasParams(b.msa.msa_att_row),
-            col_att_name: msa_col_att_params,
-            "msa_transition": MSATransitionParams(b.msa.msa_transition),
-            "outer_product_mean": OuterProductMeanParams(b.communication),
-            "triangle_multiplication_outgoing": TriMulOutParams(b.pair.tri_mul_out),
-            "triangle_multiplication_incoming": TriMulInParams(b.pair.tri_mul_in),
-            "triangle_attention_starting_node": TriAttParams(b.pair.tri_att_start),
-            "triangle_attention_ending_node": TriAttParams(b.pair.tri_att_end),
-            "pair_transition": PairTransitionParams(b.pair.pair_transition),
-        }
-
-        return d
-
-    ExtraMSABlockParams = partial(EvoformerBlockParams, is_extra_msa=True)
-
-    FoldIterationParams = lambda sm: {
-        "invariant_point_attention": IPAParams(sm.ipa),
-        "attention_layer_norm": LayerNormParams(sm.layer_norm_ipa),
-        "transition": LinearParams(sm.transition.layers[0].linear_1),
-        "transition_1": LinearParams(sm.transition.layers[0].linear_2),
-        "transition_2": LinearParams(sm.transition.layers[0].linear_3),
-        "transition_layer_norm": LayerNormParams(sm.transition.layer_norm),
-        "affine_update": LinearParams(sm.bb_update.linear),
-        "rigid_sidechain": {
-            "input_projection": LinearParams(sm.angle_resnet.linear_in),
-            "input_projection_1": LinearParams(sm.angle_resnet.linear_initial),
-            "resblock1": LinearParams(sm.angle_resnet.layers[0].linear_1),
-            "resblock2": LinearParams(sm.angle_resnet.layers[0].linear_2),
-            "resblock1_1": LinearParams(sm.angle_resnet.layers[1].linear_1),
-            "resblock2_1": LinearParams(sm.angle_resnet.layers[1].linear_2),
-            "unnormalized_angles": LinearParams(sm.angle_resnet.linear_out),
-        },
-    }
-
     ############################
     # Translations dict overflow
     ############################
@@ -406,7 +411,7 @@ def import_jax_weights_(
     # Generate translation dictionary
     translations = generate_translation_dict(
         model,
-        enable_template=use_template,
+        enable_template=False,  # use_template, # TODO: Remove
         enable_ptm=use_ptm,
     )
 
@@ -419,8 +424,11 @@ def import_jax_weights_(
     incorrect = [k for k in flat_keys if k not in keys]
     missing = [k for k in keys if k not in flat_keys]
 
-    logging.debug(f"Incorrect: {incorrect}")
-    logging.debug(f"Missing: {missing}")
+    for x in incorrect:
+        logging.debug(f"Incorrect: {x}")
+
+    for x in missing:
+        logging.debug(f"Missing: {x}")
 
     assert len(incorrect) == 0
     # assert(sorted(list(flat.keys())) == sorted(list(data.keys())))
