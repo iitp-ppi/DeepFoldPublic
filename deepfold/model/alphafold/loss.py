69629c71ece9ea343b30f5d2181ced51f2601340
vv137 <vv137.xyz@gmail.com>
2023-12-28 14:00:53 +0900

    feat: Add routine which computes FAPE

--- deepfold/model/alphafold/loss.py
+++ deepfold/model/alphafold/loss.py
@@ -34,39 +34,6 @@ def sigmoid_cross_entory(logits: torch.Tensor, labels: torch.Tensor) -> torch.Te
     return loss
 
 
-def torsion_angle_loss(a: torch.Tensor, a_gt: torch.Tensor, a_alt_gt: torch.Tensor) -> torch.Tensor:
-    """
-    Torsion angle loss.
-
-    Args:
-        a: torch.Tensor [*, N, 7, 2]
-            Cosine and sine value of angles
-        a_gt: torch.Tensor
-        a_alt_gt: torch.Tensor
-
-    Returns:
-        torch.Tensor
-    """
-    # [*, 7, 2]
-    norm = torch.norm(a, dim=-1)
-
-    # [*, N, 7, 2]
-    a = a / norm.unsqueeze(-1)
-
-    # [*, N, 7]
-    diff_norm_gt = torch.norm(a - a_gt, dim=-1)
-    diff_norm_alt_at = torch.norm(a - a_alt_gt, dim=-1)
-    min_diff = torch.minimum(diff_norm_gt**2, diff_norm_alt_at**2)
-
-    # [*]
-    loss_torsion = torch.mean(min_diff, dim=(-1, -2))
-    loss_angle_norm = torch.mean(torch.abs(norm - 1), dim=(-1, -2))
-
-    angle_weight = 0.02
-
-    return loss_torsion * angle_weight * loss_angle_norm
-
-
 def compute_plddt(logits: torch.Tensor) -> torch.Tensor:
     num_bins = logits.shape[-1]
     bin_width = 1.0 / num_bins
@@ -106,16 +73,20 @@ def compute_predicted_aligned_error(
     """Computes aligned confidence metrics from logits.
 
     Args:
-      logits: [*, N', N, num_bins] the logits output from
-        PredictedAlignedErrorHead.
-      max_bin: Maximum bin value
-      num_bins: Number of bins
+        logits: [*, N', N, num_bins]
+            the logits output from PredictedAlignedErrorHead
+        max_bin:
+            Maximum bin value
+        num_bins:
+            Number of bins
+
     Returns:
-      aligned_confidence_probs: [*, N', N, num_bins] the predicted
-        aligned error probabilities over bins for each residue pair.
-      predicted_aligned_error: [*, N', N] the expected aligned distance
-        error for each pair of residues.
-      max_predicted_aligned_error: [*] the maximum predicted error possible.
+        aligned_confidence_probs: [*, N', N, num_bins]
+            the predicted aligned error probabilities over bins for each residue pair
+        predicted_aligned_error: [*, N', N]
+            the expected aligned distance error for each pair of residues.
+        max_predicted_aligned_error: [*]
+            the maximum predicted error possible.
     """
     boundaries = torch.linspace(0, max_bin, steps=(num_bins - 1), device=logits.device)
 
@@ -166,3 +137,137 @@ def compute_tm(
 
     argmax = (weighted == torch.max(weighted)).nonzero()[0]
     return per_alignment[tuple(argmax)]
+
+
+def torsion_angle_loss(a: torch.Tensor, a_gt: torch.Tensor, a_alt_gt: torch.Tensor) -> torch.Tensor:
+    """
+    Torsion angle loss.
+
+    Args:
+        a: torch.Tensor [*, N, 7, 2]
+            Cosine and sine value of angles
+        a_gt: torch.Tensor
+        a_alt_gt: torch.Tensor
+
+    Returns:
+        torch.Tensor
+    """
+    # [*, 7, 2]
+    norm = torch.norm(a, dim=-1)
+
+    # [*, N, 7, 2]
+    a = a / norm.unsqueeze(-1)
+
+    # [*, N, 7]
+    diff_norm_gt = torch.norm(a - a_gt, dim=-1)
+    diff_norm_alt_at = torch.norm(a - a_alt_gt, dim=-1)
+    min_diff = torch.minimum(diff_norm_gt**2, diff_norm_alt_at**2)
+
+    # [*]
+    loss_torsion = torch.mean(min_diff, dim=(-1, -2))
+    loss_angle_norm = torch.mean(torch.abs(norm - 1), dim=(-1, -2))
+
+    angle_weight = 0.02
+
+    return loss_torsion * angle_weight * loss_angle_norm
+
+
+def compute_fape(
+    pred_frames: Rigid,
+    target_frames: Rigid,
+    frames_mask: torch.Tensor,
+    pred_positions: torch.Tensor,
+    target_positions: torch.Tensor,
+    positions_mask: torch.Tensor,
+    length_scale: float,
+    l1_clamp_distance: Optional[float] = None,
+    eps: float = 1e-8,
+) -> torch.Tensor:
+    """
+    Compute FAPE loss.
+
+    Args:
+        pred_frames: [*, N_frames]
+            Rigid object of predicted frames
+        target_frames: [*, N_frames]
+            Rigid objecct of ground truth frames
+        frames_mask: [*, N_frames]
+            binary mask for the frames
+        pred_positions: [*, N_pts, 3]
+            predicted atom positions
+        target_positions: [*, N_pts, 3]
+            ground truth positions
+        positions_mask: [*, N_pts]
+            positions mask
+        length_scale: float
+            length scale where the loss is divided
+        l1_clamp_distance:
+            cutoff above which distance errors are clamped
+        eps:
+            small value used to regularize denominators
+
+    Returns:
+        fape_loss: [*]
+            loss tensor
+    """
+    # [*, N_frames, N_pts, 3]
+    local_pred_pos = pred_frames.invert()[..., None].apply(pred_positions[..., None, :, :])
+    local_target_pos = target_frames.invert()[..., None].apply(target_positions[..., None, :, :])
+    error_dist = torch.sqrt(torch.sum((local_pred_pos - local_target_pos) ** 2, dim=-1) + eps)
+
+    if l1_clamp_distance is not None:
+        error_dist = torch.clamp(error_dist, min=0.0, max=l1_clamp_distance)
+
+    normed_error = error_dist / length_scale
+    normed_error = normed_error * frames_mask[..., None]
+    normed_error = normed_error * positions_mask[..., None, :]
+
+    normed_error = torch.sum(normed_error, dim=-1)
+    normed_error = normed_error / (eps + torch.sum(frames_mask, dim=-1))[..., None]
+    normed_error = torch.sum(normed_error, dim=-1)
+    normed_error = normed_error / (eps + torch.sum(positions_mask, dim=-1))
+
+    return normed_error
+
+
+# backbone_loss
+
+# sidechain_loss
+
+# fape_loss
+
+# supervised_chi_loss
+
+# lddt
+
+# lddt_ca
+
+# lddt_loss
+
+# distogram_loss
+
+# tm_loss
+
+# between_residue_bond_loss
+
+# between_residue_clash_loss
+
+# within_residue_violations
+
+# find_structural_violations
+
+# find_structural_violations
+
+# extreme_ca_ca_distance_violations
+
+# compute_violation_metrics
+
+# violation_loss
+
+# compute_renamed_ground_truth
+
+# experimentally_resolved_loss
+
+# masked_msa_loss
+
+# AlphaFoldLoss
