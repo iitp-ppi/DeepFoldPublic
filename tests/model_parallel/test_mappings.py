0eff4b33e923afb90435419cbb8578efb301b3c2
vv137 <vv137.xyz@gmail.com>
2024-01-31 21:32:44 +0900

    feat: Unit tests for mappings

--- tests/model_parallel/test_mappings.py
+++ tests/model_parallel/test_mappings.py
@@ -22,7 +22,7 @@ class TestModelParallelMappings(MultiProcessTestCase):
     def world_size(self):
         return 2
 
-    # @unittest.skip("")
+    @unittest.skip("")
     def test_CopyToModelParallelRegion(self):
         Distributed.initialize_model_parallel(self.world_size, self.rank, NGPUS)
         x1 = torch.ones((1)).cuda() * self.rank
@@ -34,7 +34,7 @@ class TestModelParallelMappings(MultiProcessTestCase):
         self.assertTrue(torch.equal(x1.grad, g1))
         Distributed.destroy_model_parallel()
 
-    # @unittest.skip("")
+    @unittest.skip("")
     def test_ReduceFromModelParallelRegion(self):
         Distributed.initialize_model_parallel(self.world_size, self.rank, NGPUS)
         x1 = torch.ones((1)).cuda() * self.rank
@@ -46,7 +46,7 @@ class TestModelParallelMappings(MultiProcessTestCase):
         self.assertTrue(torch.equal(torch.ones_like(x1), x1.grad))
         Distributed.destroy_model_parallel()
 
-    # @unittest.skip("")
+    @unittest.skip("")
     def test_ScatterToModelParallelRegion(self):
         Distributed.initialize_model_parallel(self.world_size, self.rank, NGPUS)
         x1 = torch.arange(8 * self.world_size, dtype=torch.float32).reshape(8, -1).cuda()
@@ -55,6 +55,32 @@ class TestModelParallelMappings(MultiProcessTestCase):
         self.assertTrue(torch.equal(y2, x1.chunk(self.world_size, dim=-1)[self.rank]))
         y2.sum().backward()
         self.assertTrue(torch.equal(x1.grad, torch.ones_like(x1)))
+        Distributed.destroy_model_parallel()
+
+    @unittest.skip("")
+    def test_GatherFromModelParallelRegion(self):
+        Distributed.initialize_model_parallel(self.world_size, self.rank, NGPUS)
+        x1 = torch.ones((16, self.world_size)).cuda()
+        x1.requires_grad_()
+        y1 = mappings.scatter_to_model_parallel_region(x1)
+        self.assertTrue(torch.equal(y1, x1.new_ones((16, 1))))
+        y1.sum().backward()
+        self.assertTrue(torch.equal(x1.grad, torch.ones_like(x1)))
+        Distributed.destroy_model_parallel()
+
+    # @unittest.skip("")
+    def test_TransposeOnModelParallelRegion(self):
+        Distributed.initialize_model_parallel(self.world_size, self.rank, NGPUS)
+        x1 = torch.ones(2, self.world_size, 3).cuda() * self.rank
+        # x2: dim 1 chunked
+        x2 = x1.chunk(self.world_size, dim=1)[self.rank]
+        x2.requires_grad_()
+        x3 = x2 * x2
+        # y1: dim 0 chunked
+        y1 = mappings.transpose_on_model_parallel_region(x3, 1, 0)
+        y1.sum().backward()
+        g2 = x2 * 2
+        self.assertTrue(torch.equal(x2.grad, g2))
         Distributed.destroy_model_parallel()
 
 
