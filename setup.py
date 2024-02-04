d0d5952038ac778593084c70a2611301b42be692
Minsoo Kim <vv137.xyz@gmail.com>
2024-02-04 19:10:29 +0900

    feat: Fork DeepSpeed's evoformer attention

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 """Setup for pip package."""
 
 import importlib.util
-import os
 
 import setuptools
+import torch
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 spec = importlib.util.spec_from_file_location("package_info", "deepfold/package_info.py")
 package_info = importlib.util.module_from_spec(spec)
@@ -15,14 +16,53 @@ __package_name__ = package_info.__package_name__
 __version__ = package_info.__version__
 
 
-def req_file(filename: str, folder: str = "deepfold"):
-    """Read requiremnts file."""
-    with open(os.path.join(folder, filename), encoding="utf-8") as f:
-        content = f.readlines()
-    return [x.strip() for x in content]
+ext_modules = []
+generator_flags = []
+
+major = torch.cuda.get_device_properties(0).major
+minor = torch.cuda.get_device_properties(0).minor
+gpu_arch = f"{major}{minor}"
+
+cc_flag = []
+compute_capabililties = [gpu_arch]
+
+cc_flag = []
+compute_capabililties = [gpu_arch]
+
+for cap in compute_capabililties:
+    cc_flag.append(f"-gencode=arch=compute_{cap},code=sm_{cap}")
+    cc_flag.append(f"-gencode=arch=compute_{cap},code=compute_{cap}")
 
 
-# install_requires = req_file("requirements.txt")
+sources = [
+    "csrc/evoformer_attn/attention.cpp",
+    "csrc/evoformer_attn/attention_back.cu",
+    "csrc/evoformer_attn/attention_cu.cu",
+]
+
+
+ext_modules.append(
+    CUDAExtension(
+        name="deepfold_kernel",
+        sources=sources,
+        extra_compile_args={
+            "cxx": ["-O2", "-std=c++17", *generator_flags],
+            "nvcc": [
+                "-O2",
+                "-std=c++17",
+                "--use_fast_math",
+                f"-DGPU_ARCH={gpu_arch}",
+                *generator_flags,
+                *cc_flag,
+            ],
+        },
+        include_dirs=[
+            "csrc/cutlass/include",
+        ],
+        optional=True,
+    )
+)
+
 
 setuptools.setup(
     name=__package_name__,
@@ -41,4 +81,6 @@ setuptools.setup(
     packages=["deepfold"],
     # install_requires=install_requires,
     include_package_data=True,
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
 )
