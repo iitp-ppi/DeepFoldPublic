fb7b4d4e19794e9a4777583110a3961ab93d2f49
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-04 11:08:50 +0900

    refactor: Reformat

--- deepfold/utils/precision.py
+++ deepfold/utils/precision.py
@@ -4,7 +4,4 @@ import torch
 
 
 def is_fp16_enabled() -> bool:
-    fp16_enabled = torch.get_autocast_gpu_dtype() == torch.float16
-    fp16_enabled = fp16_enabled and torch.is_autocast_enabled()
-
-    return fp16_enabled
+    return torch.is_autocast_enabled() and torch.get_autocast_gpu_dtype() == torch.float16
