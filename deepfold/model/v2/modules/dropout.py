3b5d64628da917db25269a28ed0dff480bede244
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-04 09:45:38 +0900

    feat: Dropout layers

--- deepfold/model/v2/modules/dropout.py
+++ deepfold/model/v2/modules/dropout.py
@@ -1,8 +1,11 @@
-from functools import partialmethod
-from typing import List, Union
+from typing import Union, Tuple, Optional
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+import deepfold.core.parallel_state as ps
+import deepfold.core.model_parallel.mappings as cc
 
 
 class Dropout(nn.Module):
@@ -11,38 +14,42 @@ class Dropout(nn.Module):
     along a particular dimension.
 
     If not in training mode, this module computes the identity function.
+
+    Supplementary '1.11.6 Dropout details'.
     """
 
-    def __init__(self, r: float, batch_dim: Union[int, List[int]]):
-        """
-        Args:
-            r:
-                Dropout rate
-            batch_dim:
-                Dimension(s) along which the dropout mask is shared
-        """
-        super(Dropout, self).__init__()
+    def __init__(
+        self,
+        p: float,
+        share_dim: Union[int, Tuple[int, ...]] = (),
+        inplace: bool = False,
+    ) -> None:
+        super().__init__()
+        assert 0.0 <= p <= 1.0
+        self.p = p
+        if type(share_dim) == int:
+            share_dim = (share_dim,)
+        else:
+            assert isinstance(share_dim, tuple)
+        self.share_dim = tuple(share_dim)
+        self.inplace = inplace
 
-        self.r = r
-        if type(batch_dim) == int:
-            batch_dim = [batch_dim]
-        self.batch_dim = batch_dim
-        self.dropout = nn.Dropout(self.r)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            x:
-                Tensor to which dropout is applied. Can have any shape
-                compatible with self.batch_dim
-        """
+    def forward(
+        self,
+        x: torch.Tensor,
+        add_output_to: torch.Tensor,
+        scattered_dim: Optional[int] = None,
+    ) -> torch.Tensor:
         shape = list(x.shape)
-        if self.batch_dim is not None:
-            for bd in self.batch_dim:
-                shape[bd] = 1
+        for d in self.share_dim:
+            shape[d] = 1
+        if scattered_dim is not None:
+            shape[scattered_dim] *= ps.get_model_parallel_world_size()
         mask = x.new_ones(shape)
-        mask = self.dropout(mask)
-        x *= mask
+        mask = F.dropout(input=mask, p=self.p, training=self.training, inplace=self.inplace)
+        if scattered_dim is not None:
+            mask = cc.scatter_to_model_parallel_region(mask, dim=scattered_dim)
+        x = _mul_add(x, mask, add_output_to)
         return x
 
 
@@ -53,7 +60,8 @@ class DropoutRowwise(Dropout):
     Mask shape: [1, N, C]
     """
 
-    __init__ = partialmethod(Dropout.__init__, batch_dim=-3)
+    def __init__(self, p: float) -> None:
+        super().__init__(p=p, share_dim=-3)
 
 
 class DropoutColumnwise(Dropout):
@@ -63,4 +71,25 @@ class DropoutColumnwise(Dropout):
     Mask shape: [N, 1, C]
     """
 
-    __init__ = partialmethod(Dropout.__init__, batch_dim=-2)
+    def __init__(self, p: float) -> None:
+        super().__init__(p=p, share_dim=-2)
+
+
+def _mul_add_native(
+    x: torch.Tensor,
+    mask: torch.Tensor,
+    y: torch.Tensor,
+) -> torch.Tensor:
+    return y + x * mask
+
+
+# _mul_add_jit = torch.compile(_mul_add_native)
+
+
+def _mul_add(
+    x: torch.Tensor,
+    mask: torch.Tensor,
+    y: torch.Tensor,
+) -> torch.Tensor:
+    _mul_add_fn = _mul_add_native
+    return _mul_add_fn(x, mask, y)
