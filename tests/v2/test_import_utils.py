9a0d7a965a1275ee559a8d6748094ab04f9bf0ae
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-12 13:19:28 +0900

    feat: AF Jax parameter import utils

---
+++ tests/v2/test_import_utils.py
@@ -0,0 +1,65 @@
+import unittest
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from deepfold.model.v2.config import AlphaFoldConfig
+from deepfold.model.v2.import_utils import import_jax_weights_
+from deepfold.model.v2.modules.alphafold import AlphaFold
+
+
+class TestImportWeights(unittest.TestCase):
+    def test_import_jax_weights_(self):
+        npz_path = Path("/scratch/alphafold.data/params/params_model_1_ptm.npz")
+
+        config = AlphaFoldConfig.from_preset(
+            is_multimer=False,
+            enable_ptm=True,
+            enable_templates=True,
+        )
+        model = AlphaFold(config=config)
+        model.eval()
+
+        import_jax_weights_(
+            model=model,
+            npz_path=str(npz_path),
+            is_multimer=False,
+            enable_ptm=True,
+            enable_templates=True,
+            fuse_tri_mul=False,
+        )
+
+        data = np.load(npz_path)
+        prefix = "alphafold/alphafold_iteration/"
+
+        test_pairs = [
+            # Normal linear weight
+            (
+                torch.as_tensor(data[prefix + "structure_module/initial_projection//weights"]).transpose(-1, -2),
+                model.structure_module.linear_in.weight,
+            ),
+            # Normal layer norm param
+            (
+                torch.as_tensor(
+                    data[prefix + "evoformer/prev_pair_norm//offset"],
+                ),
+                model.recycling_embedder.layer_norm_z.bias,
+            ),
+            # From a stack
+            (
+                torch.as_tensor(
+                    data[prefix + ("evoformer/evoformer_iteration/outer_product_mean/" "left_projection//weights")][
+                        1
+                    ].transpose(-1, -2)
+                ),
+                model.evoformer_stack.blocks[1].outer_product_mean.linear_1.weight,
+            ),
+        ]
+
+        for w_alpha, w_repro in test_pairs:
+            self.assertTrue(torch.all(w_alpha == w_repro))
+
+
+if __name__ == "__main__":
+    unittest.main()
