1e1a3d9f614651a636291ebd1ea0285e0b4b72ec
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-18 12:27:54 +0900

    fix: Multimer MSA search

--- scripts/search_multimer.sh
+++ scripts/search_multimer.sh
@@ -1,17 +1,19 @@
-#/bin/bash -ex
+#!/bin/bash
 
 FASTA_PATH=$1
 OUTPUT_DIR=$2
-DB_BASE=${DATABASE_BASE:-"/scratch/database/casp16"}
+DATABASE_BASE=${DATABASE_BASE:-"/scratch/database/casp16"}
 
 echo "IN=$FASTA_PATH"
 echo "OUT=$OUTPUT_DIR"
-echo "DB_BASE=$DB_BASE"
+echo "DATABASE_BASE=$DATABASE_BASE"
+echo "HOSTNAME=$(hostname)"
+echo
 
 NUM_CPUS=${NUM_CPUS:-SLURM_CPUS_PER_TASK}
 OMP_NUM_THREADS=$NUM_CPUS
 
-function run_jackhmmer {
+function run_jackhmmer() {
     local NAME=$1
     local INPUT_FASTA_PATH=$2
     local DB_PATH=$3
@@ -20,16 +22,18 @@ function run_jackhmmer {
     echo "TOOL=JackHMMER"
     echo "DB=$DB_PATH"
 
-    jackhmmer \
+    time jackhmmer \
+        --cpu $NUM_CPUS \
         -o /dev/null \
         -A $STO_PATH --noali \
         --incE 0.0001 --F1 0.0005 --F2 0.00005 --F3 0.0000005 \
         -N 1 -E 0.0001 \
-        --cpu $NUM_CPUS \
-        $INPUT_FASTA_PATH $DB_PATH 2>&1 >"$OUTPUT_DIR/$NAME.log"
+        $INPUT_FASTA_PATH $DB_PATH 2>&1 | tee "$OUTPUT_DIR/$NAME.log"
+
+    echo
 }
 
-function run_hhblits {
+function run_hhblits() {
     local NAME=$1
     local INPUT_FASTA_PATH=$2
     local DB_PATH=$3
@@ -38,9 +42,9 @@ function run_hhblits {
     echo "TOOL=HHBlits"
     echo "DB=$DB_PATH"
 
-    hhblits \
-        -i $INPUT_FASTA_PATH \
+    time hhblits \
         -cpu $NUM_CPUS \
+        -i $INPUT_FASTA_PATH \
         -oa3m $A3M_PATH \
         -o /dev/null \
         -n 3 -e 0.0001 \
@@ -48,8 +52,16 @@ function run_hhblits {
         -maxfilt 100000 \
         -min_prefilter_hits 1000 \
         -maxseq 1000000 \
-        -d $DB_PATH 2>&1 >"$OUTPUT_DIR/$NAME.log"
+        -cpu $NUM_CPUS \
+        -d $DB_PATH 2>&1 | tee "$OUTPUT_DIR/$NAME.log"
+
+    echo
 }
+
+# Check arguments
+if [ $# -ne 2 ]; then
+    echo "Illegal number of parameters: $# != 2"
+fi
 
 if ! command -v hhblits &>/dev/null; then
     echo "Cannot find HHBlits..."
@@ -65,39 +77,36 @@ else
     echo "JACKHMMER_PATH='$(which jackhmmer)'"
 fi
 
-# Check arguments
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters: $# != 2"
-fi
-
 # Check input
 if ! [ -f "$FASTA_PATH" ]; then
     echo "Cannot find input: '$FASTA_PATH'"
     exit 1
 fi
 
+echo
+
 # Prepare output directory
 mkdir -p $OUTPUT_DIR
 
 # UniRef90
-run_jackhmmer "uniref90" $INPUT_FASTA_PATH "${DB_BASE}/uniref90/uniref90.fasta"
+run_jackhmmer "uniref90" $FASTA_PATH "${DATABASE_BASE}/uniref90/uniref90.fasta"
 
 # MGnify
-run_jackhmmer "mgnify" $INPUT_FASTA_PATH "${DB_BASE}/mgnify/mgy_clusters_2022_05.fa"
+run_jackhmmer "mgnify" $FASTA_PATH "${DATABASE_BASE}/mgnify/mgy_clusters_2022_05.fa"
 
 # UniProt
-run_jackhmmer "uniprot" $INPUT_FASTA_PATH "${DB_BASE}/uniprot/uniprot.fasta"
+run_jackhmmer "uniprot" $FASTA_PATH "${DATABASE_BASE}/uniprot/uniprot.fasta"
 
 # BFD
-run_hhblits "bfd" $INPUT_FASTA_PATH "${DB_BASE}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
+run_hhblits "bfd" $FASTA_PATH "${DATABASE_BASE}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
 
 # UniRef30
-run_hhblits "uniref30" $INPUT_FASTA_PATH "${DB_BASE}/uniref30/UniRef30_2021_03/UniRef30_2021_03"
+run_hhblits "uniref30" $FASTA_PATH "${DATABASE_BASE}/uniref30/UniRef30_2021_03/UniRef30_2021_03"
 
 # PDB70
-# run_hhblits "pdb70" $INPUT_FASTA_PATH "${DB_BASE}/pdb70/pdb70"
+# run_hhblits "pdb70" $FASTA_PATH "${DATABASE_BASE}/pdb70/pdb70"
 
 # UniClust30
-# run_jachmmer "uniclust30" $INPUT_FASTA_PATH "${DB_BASE/uniclust/...}"
+# run_jachmmer "uniclust30" $FASTA_PATH "${DATABASE_BASE/uniclust/...}"
 
 exit 0
