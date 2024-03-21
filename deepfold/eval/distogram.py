c01754100638e482bf151b065224c8547ffbc592
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-21 15:26:27 +0900

    feat: Plot utils

---
+++ deepfold/eval/distogram.py
@@ -0,0 +1,67 @@
+from typing import Dict, Optional
+
+import numpy as np
+from scipy.special import softmax
+
+
+def bin_edges_np(
+    min_bin: float,
+    max_bin: float,
+    num_bins: int,
+) -> np.ndarray:
+    bin_edges = np.linspace(
+        start=min_bin,
+        stop=max_bin,
+        num=num_bins,
+    )
+
+    return bin_edges
+
+
+def calculate_bin_centers_np(boundaries: np.ndarray) -> np.ndarray:
+    step = boundaries[1] - boundaries[0]
+    bin_centers = boundaries + step * 0.5
+    bin_centers = np.concatenate(
+        [
+            bin_centers,
+            (bin_centers[-1] + step)[..., None],
+        ],
+        axis=0,
+    )
+    return bin_centers
+
+
+def undigitize(indices: np.ndarray, bins: np.ndarray, right: bool = False) -> np.ndarray:
+    if np.any(indices < 0) or np.any(indices >= len(bins)):
+        raise ValueError("Indices must be within the range of bins")
+
+    widths = np.diff(bins)
+
+    if right:
+        bins = bins - widths
+
+    return bins[indices]
+
+
+def compute_predicted_distogram(
+    logits: np.ndarray,
+    min_bin: float = 2.2325,
+    max_bin: float = 21.6875,
+    num_bins: int = 64,
+) -> np.ndarray:
+    boundaries = bin_edges_np(min_bin, max_bin, num_bins - 1)
+    bin_centers = calculate_bin_centers_np(boundaries)
+    probs = softmax(logits, axis=-1)
+    predicted_distoram = np.sum(probs * bin_centers, axis=-1)
+    return predicted_distoram
+
+
+def compute_distogram(
+    pts: np.ndarray,
+    min_bin: float = 2.2325,
+    max_bin: float = 21.6875,
+    num_bins: int = 64,
+) -> np.ndarray:
+    boundaries = bin_edges_np(min_bin, max_bin, num_bins)
+    distances = np.sqrt(np.sum((pts[..., None, :, :] - pts[..., :, None, :]) ** 2, axis=-1))
+    return distances
---
+++ deepfold/launch/plot.py
@@ -0,0 +1,247 @@
+import os
+from typing import Dict, Iterable, List, Sequence, Tuple
+
+import matplotlib as mpl
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+import deepfold.common.residue_constants as rc
+from deepfold.eval.distogram import compute_distogram, compute_predicted_distogram
+
+
+def find_cluster_boundaries(a: np.ndarray) -> List[Tuple[int, int, int]]:
+    a = np.asarray(a)
+    assert a.ndim == 1
+    boundaries = []
+    start = 0
+    current = a[0]
+
+    for i in range(1, len(a)):
+        if a[i] != current:
+            boundaries.append((current, start, i - 1))
+            start = i
+            current = a[i]
+    boundaries.append((start, len(a) - 1, current))
+
+    return boundaries
+
+
+def plot_distogram(
+    outputs: dict,
+    fig_kwargs: dict = dict(),
+) -> plt.Figure:
+    num_models = len(outputs)
+    fig_kwargs.update(
+        {
+            "figsize": (5 * num_models, 9),
+            "dpi": 150.0,
+        }
+    )
+    fig = plt.figure(**fig_kwargs)
+    for n, (model_name, value) in enumerate(outputs.items(), start=1):
+        if "asym_id" in value:
+            boundaries = find_cluster_boundaries(value["asym_id"])
+
+        # From the distogram head
+        ax = fig.add_subplot(2, num_models, n)
+        ax.set_title(f"{model_name} from the Distogram Head")
+        distogram = compute_predicted_distogram(value["distogram_logits"])
+        im1 = ax.imshow(distogram, cmap="viridis_r", vmin=0, vmax=22)
+
+        # Draw chain breaks
+        if "asym_id" in value:
+            for i, _, _ in boundaries[1:]:
+                ax.axhline(y=i, color="k", linestyle="-", alpha=0.6)
+                ax.axvline(x=i, color="k", linestyle="-", alpha=0.6)
+
+        # Pseudo beta positions
+        all_atom_positions = value["final_atom_positions"]
+        all_atom_mask = value["final_atom_mask"]
+        ca_idx = rc.atom_order["CA"]
+        is_gly = all_atom_mask[:, rc.restype_order["G"]] < 0.5
+        cb_idx = rc.atom_order["CB"]
+        pseudo_beta = np.where(
+            np.tile(is_gly[..., None], [1] * len(is_gly.shape) + [3]),
+            all_atom_positions[..., ca_idx, :],
+            all_atom_positions[..., cb_idx, :],
+        )
+
+        # From the final atom positions
+        ax = fig.add_subplot(2, num_models, n + num_models)
+        ax.set_title(f"{model_name} from the Structure")
+        distogram = compute_distogram(pseudo_beta)
+        im2 = ax.imshow(np.clip(distogram, a_min=0, a_max=22), cmap="viridis_r", vmin=0, vmax=22)
+
+        # Draw chain breaks
+        if "asym_id" in value:
+            for i, _, _ in boundaries[1:]:
+                ax.axhline(y=i, color="k", linestyle="-", alpha=0.6)
+                ax.axvline(x=i, color="k", linestyle="-", alpha=0.6)
+
+    fig.colorbar(im1)
+    fig.colorbar(im2)
+
+    return fig
+
+
+def plot_predicted_alignment_error(
+    outputs: dict,
+    fig_kwargs: dict = dict(),
+) -> plt.Figure:
+    num_models = len(outputs)
+    fig_kwargs.update(
+        {
+            "figsize": (5 * num_models, 4),
+            "dpi": 150.0,
+        }
+    )
+    fig = plt.figure(**fig_kwargs)
+    for n, (model_name, value) in enumerate(outputs.items(), start=1):
+        # Draw PAE
+        ax = fig.add_subplot(1, num_models, n)
+        ax.set_title(model_name)
+        im = ax.imshow(value["predicted_aligned_error"], label=model_name, cmap="bwr", vmin=0, vmax=30)
+
+        # Draw chain breaks
+        if "asym_id" in value:
+            boundaries = find_cluster_boundaries(value["asym_id"])
+            for i, _, _ in boundaries[1:]:
+                ax.axhline(y=i, color="k", linestyle="-", alpha=0.6)
+                ax.axvline(x=i, color="k", linestyle="-", alpha=0.6)
+    fig.colorbar(im)
+
+    return fig
+
+
+PLDDT_COLORS = [
+    (0.0, "#ff7d45"),
+    (0.5, "#ffdb13"),
+    (0.7, "#65cbf3"),
+    (0.9, "#0053d6"),
+    (1.0, "#0053d6"),
+]
+
+PLDDT_CMAP = LinearSegmentedColormap.from_list(name="plddt", colors=PLDDT_COLORS)
+
+
+def plot_plddt(
+    outputs: dict,
+    scale_with_len: bool = False,
+    fig_kwargs: dict = dict(),
+) -> plt.Figure:
+    # Scale with the length
+    scale = 1
+    if scale_with_len:
+        max_len = 0
+        for v in outputs.values():
+            max_len = max(max_len, v["plddt"].shape[0])
+        scale = max(scale, 1 + max_len // 200)
+
+    fig_kwargs.update(
+        {
+            "figsize": (8 * scale, 5),
+            "dpi": 150.0,
+        }
+    )
+    fig = plt.figure(**fig_kwargs)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title(rf"Predicted CA-lDDT")
+    for n, (model_name, value) in enumerate(outputs.items(), start=1):
+        # Draw plDDT
+        x = np.arange(len(value["plddt"])) + 1
+        y = value["plddt"]
+        ax.scatter(
+            x=x,
+            y=y,
+            c=(y / 100),
+            cmap=PLDDT_CMAP,
+            marker=".",
+            zorder=2,
+        )
+        ax.plot(x, y, "-", label=f"rank_{n} ({model_name})", zorder=1)
+
+        # Draw chain breaks
+        if "asym_id" in value:
+            boundaries = find_cluster_boundaries(value["asym_id"])
+            for i, _, _ in boundaries[1:]:
+                ax.axvline(x=i, color="k", linestyle="-.")
+    ax.legend()
+    ax.set_ylim(0, 100)
+    ax.set_xlabel("Positions")
+    ax.set_ylabel("plDDT")
+
+    return fig
+
+
+def plot_msa(
+    feature_dict: Dict[str, np.ndarray],
+    sort_lines: bool = True,
+    dpi: float = 150.0,
+) -> plt.Figure:
+    seq = feature_dict["msa"][0]
+    if "asym_id" in feature_dict:  # Multimer
+        ls = [0]
+        k = feature_dict["asym_id"][0]
+        for i in feature_dict["asym_id"]:
+            if i == k:
+                ls[-1] += 1
+            else:
+                ls.append(1)
+            k = i
+    else:
+        ls = [len(seq)]
+    ln = np.cumsum([0] + ls)
+
+    try:
+        n = feature_dict["num_alignments"][0]
+    except:
+        n = feature_dict["num_alignments"]
+
+    msa = feature_dict["msa"][:n]
+    gap = msa != 21
+    qid = msa == seq
+    gapid = np.stack([gap[:, ln[i] : ln[i + 1]].max(-1) for i in range(len(ls))], -1)
+    lines = []
+    nn = []
+    for g in np.unique(gapid, axis=0):
+        i = np.where((gapid == g).all(axis=-1))
+        qid_ = qid[i]
+        gap_ = gap[i]
+        seqid = np.stack([qid_[:, ln[i] : ln[i + 1]].mean(-1) for i in range(len(ls))], -1).sum(-1) / (g.sum(-1) + 1e-8)
+        non_gaps = gap_.astype(float)
+        non_gaps[non_gaps == 0] = np.nan
+        if sort_lines:
+            lines_ = non_gaps[seqid.argsort()] * seqid[::-1, None]
+        else:
+            lines_ = non_gaps[::-1] * seqid[::-1, None]
+        nn.append(len(lines_))
+        lines.append(lines_)
+
+    nn = np.cumsum(np.append(0, nn))
+    lines = np.concatenate(lines, 0)
+    fig = plt.figure(figsize=(8, 5), dpi=dpi)
+    ax = fig.add_subplot(1, 1, 1)
+    im = ax.imshow(
+        lines,
+        cmap="rainbow_r",
+        interpolation="nearest",
+        aspect="auto",
+        vmax=1.0,
+        vmin=0.0,
+        origin="lower",
+        extent=(0, lines.shape[1], 0, lines.shape[0]),
+    )
+    for i in ln[1:-1]:
+        ax.plot([i, i], [0, lines.shape[0]], color="black")
+    for j in nn[1:-1]:
+        ax.plot([0, lines.shape[1]], [j, j], color="black")
+
+    ax.plot((np.isnan(lines) == False).sum(0), color="black")
+    ax.set_xlim(0, lines.shape[1])
+    ax.set_ylim(0, lines.shape[0])
+    fig.colorbar(im)
+    ax.set_xlabel("Positions")
+    ax.set_ylabel("Sequences")
+
+    return fig
