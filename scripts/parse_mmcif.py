159b5945c8085c7cc430ba22786edf7e11384de2
Minsoo Kim <vv137.xyz@gmail.com>
2024-03-29 12:21:24 +0900

    feat: mmCIF parser

---
+++ scripts/parse_mmcif.py
@@ -0,0 +1,71 @@
+import json
+import logging
+import sys
+import warnings
+from pathlib import Path
+
+import tqdm
+
+from deepfold.data.pdbx_parsing import MMCIFParser, get_assembly_infos, get_chain_features, get_fasta, read_mmcif
+from deepfold.utils.file_utils import dump_pickle
+
+warnings.filterwarnings("ignore")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main_pdbx(entry_id: str):
+    entry_id = entry_id.upper()
+
+    out_dir = Path("out") / entry_id[1:3] / entry_id
+    if (out_dir / f"{entry_id}.fasta").exists():
+        return
+
+    mmcif_str = read_mmcif(entry_id, mmcif_path="/scratch/pdb/mmCIF")
+    parser = MMCIFParser()
+    o = parser.parse(mmcif_str, entry_id=entry_id)
+
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    for model_num, chains in o.mmcif_object.models.items():
+        for chain_id in chains.keys():
+            feats = get_chain_features(o.mmcif_object, model_num, chain_id)
+            dump_pickle(feats, out_dir / f"{entry_id}_{chain_id}.{model_num}.pkl")
+
+    assems = get_assembly_infos(o.mmcif_object)
+    for name, assem in assems.items():
+        assem_str = json.dumps(assem)
+        with open(out_dir / f"{name}.json", "w") as fp:
+            fp.write(assem_str)
+
+    fasta_str = get_fasta(o.mmcif_object)
+    with open(out_dir / f"{entry_id}.fasta", "w") as fp:
+        fp.write(fasta_str)
+
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    with open(sys.argv[1], "r") as fp:
+        lines = fp.read().split("\n")
+
+    with open(sys.argv[2], "a") as fp:
+        for line in (pbar := tqdm.tqdm(lines)):
+            line = line.strip()
+            pbar.set_description_str(f"PDBx: {line}")
+            if line == "":
+                continue
+            try:
+                main_pdbx(line)
+            except Exception:
+                # raise
+                fp.write(f"{line}\n")
+                continue
+            except KeyboardInterrupt:
+                fp.write(f"{line}\n")
+                continue
+
+
+if __name__ == "__main__":
+    main()
