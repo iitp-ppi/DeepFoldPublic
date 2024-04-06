8255b688ffab4c01488ee78233f1113e0ae7f2bd
vv137 <vv137.xyz@gmail.com>
2024-04-06 16:37:14 +0900

    fix: Rearrange procedure

--- scripts/search_multimer.sh
+++ scripts/search_multimer.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-##SBATCH --job-name=msa
-##SBATCH -N 1
-##SBATCH -n 1
-##SBATCH -c 16
-##SBATCH --time=1-00:00:00
-##SBATCH --partition=normal
-##SBATCH --error=slurm.%J.out
-##SBATCH --output=slurm.%J.out
+#SBATCH --job-name=msa
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=normal
 
 FASTA_PATH=$1
 OUTPUT_DIR=$2
@@ -25,6 +23,8 @@ NUM_CPUS=16
 NUM_CPUS=${SLURM_CPUS_PER_TASK:-$NUM_CPUS}
 OMP_NUM_THREADS=$NUM_CPUS
 
+echo "NUM_CPUS=$NUM_CPUS"
+
 function run_jackhmmer() {
     local NAME=$1
     local INPUT_FASTA_PATH=$2
@@ -34,13 +34,19 @@ function run_jackhmmer() {
     echo "TOOL=JACKHMMER"
     echo "DB=$DB_PATH"
 
+    if [ -s $STO_PATH ]; then
+        echo "File exists and is not empty: $STO_PATH"
+        echo
+        return
+    fi
+
     time jackhmmer \
         --cpu $NUM_CPUS \
         -o /dev/null \
         -A $STO_PATH --noali \
         --incE 0.0001 --F1 0.0005 --F2 0.00005 --F3 0.0000005 \
         -N 1 -E 0.0001 \
-        $INPUT_FASTA_PATH $DB_PATH > /dev/null 2>&2
+        $INPUT_FASTA_PATH $DB_PATH >/dev/null 2>&1
 
     echo
 }
@@ -54,6 +60,12 @@ function run_hhblits() {
     echo "TOOL=HHBLITS"
     echo "DB=$DB_PATH"
 
+    if [ -s $A3M_PATH ]; then
+        echo "File exists and is not empty: $A3M_PATH"
+        echo
+        return
+    fi
+
     time hhblits \
         -cpu $NUM_CPUS \
         -i $INPUT_FASTA_PATH \
@@ -65,7 +77,7 @@ function run_hhblits() {
         -min_prefilter_hits 1000 \
         -maxseq 1000000 \
         -cpu $NUM_CPUS \
-        -d $DB_PATH  > /dev/null 2>&2
+        -d $DB_PATH >/dev/null 2>&1
 
     echo
 }
@@ -80,10 +92,16 @@ function run_hmmsearch() {
     echo "QUERY=$INPUT_STO_PATH"
     echo "DB=$DB_PATH"
 
+    if [ -s $STO_PATH ]; then
+        echo "File exists and is not empty: $STO_PATH"
+        echo
+        return
+    fi
+
     time hmmbuild \
         --hand --amino \
         $HMM_PATH \
-        $INPUT_STO_PATH
+        $INPUT_STO_PATH >/dev/null 2>&1
 
     time hmmsearch \
         --noali --cpu $NUM_CPUS \
@@ -91,7 +109,7 @@ function run_hmmsearch() {
         --incE 100 -E 100 --domE 100 --incdomE 100 \
         -A $STO_PATH \
         $HMM_PATH \
-        $DB_PATH  > /dev/null 2>&2
+        $DB_PATH >/dev/null 2>&1
 
     rm -f $HMM_PATH
 
@@ -107,6 +125,12 @@ function run_hhsearch() {
     echo "TOOL=HHSEARCH"
     echo "DB=$DB_PATH"
 
+    if [ -s $HHR_PATH ]; then
+        echo "File exists and is not empty: $HHR_PATH"
+        echo
+        return
+    fi
+
     $REFORMAT sto a3m $INPUT_STO_PATH $A3M_PATH
 
     time hhsearch \
@@ -114,7 +138,7 @@ function run_hhsearch() {
         -o $HHR_PATH \
         -maxseq 1000000 \
         -cpu $NUM_CPUS \
-        -d $DB_PATH  > /dev/null 2>&2
+        -d $DB_PATH >/dev/null 2>&1
 
     rm -f $A3M_PATH
 
@@ -122,8 +146,8 @@ function run_hhsearch() {
 }
 
 # Check arguments
-if [ $# -ne 2 ]; then
-    echo "Illegal number of parameters: $# != 2"
+if [ $# -ne 1 ]; then
+    echo "Illegal number of parameters: $# != 1"
 fi
 
 if ! command -v hhblits &>/dev/null; then
@@ -149,19 +173,15 @@ fi
 echo
 
 # Prepare output directory
-mkdir -p $OUTPUT_DIR
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
 
 # UniRef30
 run_hhblits "uniref30" $FASTA_PATH "${DATABASE_BASE}/uniref30/UniRef30_2021_03/UniRef30_2021_03"
 
 # UniRef90
 run_jackhmmer "uniref90" $FASTA_PATH "${DATABASE_BASE}/uniref90/uniref90.fasta"
-
-# HMM
-run_hmmsearch "${OUTPUT_DIR}/uniref90_hits.sto" "${DATABASE_BASE}/pdb/pdb_seqres.txt"
-
-# PDB70
-run_hhsearch "${OUTPUT_DIR}/uniref90_hits.sto" "${DATABASE_BASE}/pdb70/pdb70"
 
 # UniProt
 run_jackhmmer "uniprot" $FASTA_PATH "${DATABASE_BASE}/uniprot/uniprot.fasta"
@@ -171,5 +191,11 @@ run_jackhmmer "mgnify" $FASTA_PATH "${DATABASE_BASE}/mgnify/mgy_clusters_2022_05
 
 # BFD
 run_hhblits "bfd" $FASTA_PATH "${DATABASE_BASE}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
+
+# HMM
+run_hmmsearch "${OUTPUT_DIR}/uniref90_hits.sto" "${DATABASE_BASE}/pdb/pdb_seqres.txt"
+
+# PDB70
+run_hhsearch "${OUTPUT_DIR}/uniref90_hits.sto" "${DATABASE_BASE}/pdb70/pdb70"
 
 exit 0
