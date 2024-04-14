febfbad1b4a584997755b969f6cc6b58ad62c7dc
vv137 <vv137.xyz@gmail.com>
2024-04-14 16:51:27 +0900

    feat: Utilities for cross entropy losses

---
+++ deepfold/losses/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def softmax_cross_entropy(logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    """Softmax cross entropy."""
+    loss = -1 * torch.sum(labels * torch.nn.functional.log_softmax(logits.float(), dim=-1), dim=-1)
+    return loss
+
+
+def sigmoid_cross_entropy(logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    logits = logits.float()
+    log_p = torch.nn.functional.logsigmoid(logits)
+    log_not_p = torch.nn.functional.logsigmoid(-logits)  # More numerically stable.
+    loss = -labels * log_p - (1 - labels) * log_not_p
+    return loss
