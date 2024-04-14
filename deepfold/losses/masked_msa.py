d0786bf69ce19c2fac9c7c50b62be9557fc1ad66
vv137 <vv137.xyz@gmail.com>
2024-04-15 00:23:58 +0900

    feat: Optimize masked MSA loss for lower precision

--- deepfold/losses/masked_msa.py
+++ deepfold/losses/masked_msa.py
@@ -2,11 +2,36 @@ import torch
 import torch.nn.functional as F
 
 from deepfold.losses.utils import softmax_cross_entropy
-from deepfold.utils.tensor_utils import masked_mean
 
 
-def masked_msa_loss(logits: torch.Tensor, true_msa: torch.Tensor, bert_mask: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
-    bert_mask = bert_mask.float()
-    errors = softmax_cross_entropy(logits.float(), F.one_hot(true_msa.long(), num_classes=logits.shape[-1]))
-    loss = masked_mean(bert_mask, errors, dim=(-1, -2), eps=eps)
+def masked_msa_loss(
+    logits: torch.Tensor,
+    true_msa: torch.Tensor,
+    bert_mask: torch.Tensor,
+    eps: float = 1e-8,
+) -> torch.Tensor:
+    """Computes BERT-style masked MSA loss.
+
+    Supplementary '1.9.9 Masked MSA prediction'.
+
+    Args:
+        logits:    [*, N_seq, N_res, 23] predicted residue distribution
+        true_msa:  [*, N_seq, N_res] true MSA
+        bert_mask: [*, N_seq, N_res] MSA mask
+
+    Returns:
+        Masked MSA loss
+
+    """
+    errors = softmax_cross_entropy(logits=logits, targets=F.one_hot(true_msa, num_classes=23))
+
+    # FP16-friendly averaging.
+    loss = errors * bert_mask
+    loss = torch.sum(loss, dim=-1)
+    scale = 0.5
+    denom = eps + torch.sum(scale * bert_mask, dim=(-1, -2))
+    loss = loss / denom[..., None]
+    loss = torch.sum(loss, dim=-1)
+    loss = loss * scale
+
     return loss
