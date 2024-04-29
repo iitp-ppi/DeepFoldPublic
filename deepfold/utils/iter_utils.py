4d8a42a7178e1f19515cd9cb8265ee2611c05f12
vv137 <vv137.xyz@gmail.com>
2024-04-29 11:18:59 +0900

    feat: Flatten dictionary

--- deepfold/utils/iter_utils.py
+++ deepfold/utils/iter_utils.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Tuple
+from typing import Callable, Iterator, MutableMapping, Tuple
 
 
 def slice_generator(start: int, end: int, size: int) -> Iterator[Tuple[int, int]]:
@@ -7,3 +7,21 @@ def slice_generator(start: int, end: int, size: int) -> Iterator[Tuple[int, int]
         left = i
         right = min(i + size, end)
         yield left, right
+
+
+def map_dict_values(fn: Callable, d: dict) -> dict:
+    """Maps dictionary values using given function."""
+    return {k: fn(v) for k, v in d.items()}
+
+
+def _flatten_dict_gen(d, parent_key, sep):
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            yield from flatten_dict(v, new_key, sep=sep).items()
+        else:
+            yield new_key, v
+
+
+def flatten_dict(d: MutableMapping, parent_key: str = "", sep: str = "."):
+    return dict(_flatten_dict_gen(d, parent_key, sep))
