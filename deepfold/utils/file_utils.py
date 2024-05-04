d4905e42b40a85f3eebc3b65fb84d2d3aa9e7dee
vv137 <vv137.xyz@gmail.com>
2024-05-04 23:01:50 +0900

    feat: PKZ format is default

--- deepfold/utils/file_utils.py
+++ deepfold/utils/file_utils.py
@@ -3,11 +3,10 @@ import gzip
 import logging
 import os
 import pickle
+import warnings
 from typing import Any
 
 __all__ = ["read_text", "load_pickle", "dump_pickle"]
-
-logger = logging.getLogger(__name__)
 
 
 def read_text(path: os.PathLike) -> str:
@@ -30,18 +29,31 @@ def _read_text(path: str) -> str:
 
 def load_pickle(path: os.PathLike) -> Any:
     try:
-        if os.path.splitext(path)[1] == ".gz":
-            with gzip.open(path, "rb") as fp:
-                return pickle.load(fp)
+        ext = os.path.splitext(path)[1]
+        if ext == ".gz":
+            _load_gz_pkl(path)
+        elif ext == ".pkz":
+            _load_pkz(path)
         else:
-            with gzip.open(f"{path}.gz", "rb") as fp:
-                return pickle.load(fp)
+            _load_pkl(path)
     except FileNotFoundError:
-        logger.debug(f"File not found: {path}")
-        with open(path, "rb") as fp:
-            return pickle.load(fp)
+        warnings.warn(f"File not found: {path}")
+
+
+def _load_gz_pkl(path: os.PathLike) -> Any:
+    with gzip.open(path, "rb") as fp:
+        return pickle.load(fp)
+
+
+def _load_pkz(path: os.PathLike) -> Any:
+    return _load_gz_pkl(path)
+
+
+def _load_pkl(path: os.PathLike) -> Any:
+    with open(path, "rb") as fp:
+        return pickle.load(path)
 
 
 def dump_pickle(obj: Any, path: os.PathLike, level: int = 6) -> None:
-    with gzip.open(f"{path}.gz", "wb", compresslevel=level) as fp:
+    with gzip.open(f"{path}.pkz", "wb", compresslevel=level) as fp:
         pickle.dump(obj, fp)
