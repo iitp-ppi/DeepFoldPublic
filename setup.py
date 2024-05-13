b6b4a3c55352ad4947f56244bd407320d939e4fd
vv137 <vv137.xyz@gmail.com>
2024-05-14 01:25:51 +0900

    build: Remove custom kernels

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 import importlib.util
 
 import setuptools
-import torch
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+# import torch
+# from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 spec = importlib.util.spec_from_file_location("package_info", "deepfold/package_info.py")
 package_info = importlib.util.module_from_spec(spec)
@@ -19,49 +20,49 @@ __version__ = package_info.__version__
 ext_modules = []
 generator_flags = []
 
-major = torch.cuda.get_device_properties(0).major
-minor = torch.cuda.get_device_properties(0).minor
-gpu_arch = f"{major}{minor}"
+# major = torch.cuda.get_device_properties(0).major
+# minor = torch.cuda.get_device_properties(0).minor
+# gpu_arch = f"{major}{minor}"
 
-cc_flag = []
-compute_capabililties = [gpu_arch]
+# cc_flag = []
+# compute_capabililties = [gpu_arch]
 
-cc_flag = []
-compute_capabililties = [gpu_arch]
+# cc_flag = []
+# compute_capabililties = [gpu_arch]
 
-for cap in compute_capabililties:
-    cc_flag.append(f"-gencode=arch=compute_{cap},code=sm_{cap}")
-    cc_flag.append(f"-gencode=arch=compute_{cap},code=compute_{cap}")
-
-
-sources = [
-    "csrc/evoformer_attn/attention.cpp",
-    "csrc/evoformer_attn/attention_back.cu",
-    "csrc/evoformer_attn/attention_cu.cu",
-]
+# for cap in compute_capabililties:
+#     cc_flag.append(f"-gencode=arch=compute_{cap},code=sm_{cap}")
+#     cc_flag.append(f"-gencode=arch=compute_{cap},code=compute_{cap}")
 
 
-ext_modules.append(
-    CUDAExtension(
-        name="deepfold_kernel",
-        sources=sources,
-        extra_compile_args={
-            "cxx": ["-O2", "-std=c++17", *generator_flags],
-            "nvcc": [
-                "-O2",
-                "-std=c++17",
-                "--use_fast_math",
-                f"-DGPU_ARCH={gpu_arch}",
-                *generator_flags,
-                *cc_flag,
-            ],
-        },
-        include_dirs=[
-            "csrc/cutlass/include",
-        ],
-        optional=True,
-    )
-)
+# sources = [
+#     "csrc/evoformer_attn/attention.cpp",
+#     "csrc/evoformer_attn/attention_back.cu",
+#     "csrc/evoformer_attn/attention_cu.cu",
+# ]
+
+
+# ext_modules.append(
+#     CUDAExtension(
+#         name="deepfold_kernel",
+#         sources=sources,
+#         extra_compile_args={
+#             "cxx": ["-O2", "-std=c++17", *generator_flags],
+#             "nvcc": [
+#                 "-O2",
+#                 "-std=c++17",
+#                 "--use_fast_math",
+#                 f"-DGPU_ARCH={gpu_arch}",
+#                 *generator_flags,
+#                 *cc_flag,
+#             ],
+#         },
+#         include_dirs=[
+#             "csrc/cutlass/include",
+#         ],
+#         optional=True,
+#     )
+# )
 
 
 setuptools.setup(
@@ -82,5 +83,5 @@ setuptools.setup(
     # install_requires=install_requires,
     include_package_data=True,
     # ext_modules=ext_modules,
-    cmdclass={"build_ext": BuildExtension},
+    # cmdclass={"build_ext": BuildExtension},
 )
