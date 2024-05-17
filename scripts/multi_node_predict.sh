b5ed0755fa8f79eeed1df2005fc17de0963e4691
vv137 <vv137.xyz@gmail.com>
2024-05-17 13:49:40 +0900

    style: reformat

--- scripts/multi_node_predict.sh
+++ scripts/multi_node_predict.sh
@@ -4,7 +4,7 @@
 #SBATCH --time       24:00:00
 #SBATCH --nodes             1
 #SBATCH --ntasks-per-node   1
-#SBATCH --output    %x-%J.out
+#SBATCH --output  %x-%06J.out
 #SBATCH --gres          gpu:4
 
 CONDA_HOME="${HOME}/conda"
