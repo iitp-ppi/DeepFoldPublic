208c1ace1a9f7eef143e12f2061fc27ca5735878
vv137 <vv137.xyz@gmail.com>
2024-05-22 18:30:42 +0900

    feat: Adjust width of plDDT

--- deepfold/eval/plot.py
+++ deepfold/eval/plot.py
@@ -1,12 +1,31 @@
-from typing import Dict, List, Optional, Sequence, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import matplotlib as mpl
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import LinearSegmentedColormap, Normalize
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import deepfold.common.residue_constants as rc
 from deepfold.eval.distogram import compute_distogram, compute_predicted_distogram
+
+
+def _set_size(
+    w: float,
+    h: float,
+    ax: plt.Axes | None = None,
+):
+    """w, h: width, height in inches"""
+    if not ax:
+        ax = plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w) / (r - l)
+    figh = float(h) / (t - b)
+    ax.figure.set_size_inches(figw, figh)
 
 
 def find_cluster_boundaries(a: np.ndarray) -> List[Tuple[int, int, int]]:
@@ -29,22 +48,32 @@ def find_cluster_boundaries(a: np.ndarray) -> List[Tuple[int, int, int]]:
 def plot_distogram(
     outputs: dict,
     asym_id: Optional[np.ndarray] = None,
+    ncols: int = 5,
+    sort: bool = False,
     fig_kwargs: dict = dict(),
 ) -> plt.Figure:
     num_models = len(outputs)
+    nrows = int((num_models + ncols + 1) / ncols) * 2
     fig_kwargs.update(
         {
-            "figsize": (5 * num_models, 9),
+            "figsize": (5 * ncols, 9 * nrows),
             "dpi": 150.0,
         }
     )
     fig = plt.figure(**fig_kwargs)
-    for n, (model_name, value) in enumerate(outputs.items(), start=1):
+
+    if sort:
+        sorted_outputs = {k: outputs[k] for k in sorted(outputs.keys())}
+    else:
+        sorted_outputs = outputs
+
+    for n, (model_name, value) in enumerate(sorted_outputs.items(), start=1):
         if asym_id is not None:
             boundaries = find_cluster_boundaries(asym_id)
 
         # From the distogram head
-        ax = fig.add_subplot(2, num_models, n)
+        m = n - 1
+        ax = fig.add_subplot(2 * nrows, ncols, 2 * (m // ncols) * ncols + m % ncols + 1)
         ax.set_title(f"{model_name} from the Distogram Head")
         distogram = compute_predicted_distogram(value["distogram_logits"])
         im1 = ax.imshow(distogram, cmap="viridis_r", vmin=0, vmax=22)
@@ -68,8 +97,12 @@ def plot_distogram(
             all_atom_positions[..., cb_idx, :],
         )
 
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im1, cax=cax, orientation="vertical", ticks=[])
+
         # From the final atom positions
-        ax = fig.add_subplot(2, num_models, n + num_models)
+        ax = fig.add_subplot(2 * nrows, ncols, 2 * (m // ncols) * ncols + ncols + m % ncols + 1)
         ax.set_title(f"{model_name} from the Structure")
         distogram = compute_distogram(pseudo_beta)
         im2 = ax.imshow(np.clip(distogram, a_min=0, a_max=22), cmap="viridis_r", vmin=0, vmax=22)
@@ -81,8 +114,9 @@ def plot_distogram(
                 ax.axhline(y=z, color="k", linestyle="-", alpha=0.6)
                 ax.axvline(x=z, color="k", linestyle="-", alpha=0.6)
 
-    fig.colorbar(im1)
-    fig.colorbar(im2)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im2, cax=cax, orientation="vertical", ticks=[])
 
     return fig
 
@@ -104,7 +138,7 @@ def plot_predicted_alignment_error(
         # Draw PAE
         ax = fig.add_subplot(1, num_models, n)
         ax.set_title(model_name)
-        im = ax.imshow(value["predicted_aligned_error"], label=model_name, cmap="bwr", vmin=0, vmax=30)
+        im = ax.imshow(value["predicted_aligned_error"], label=model_name, cmap="Greens", vmin=0, vmax=30)
 
         # Draw chain breaks
         if asym_id is not None:
@@ -113,7 +147,11 @@ def plot_predicted_alignment_error(
                 z = i - 0.5
                 ax.axhline(y=z, color="k", linestyle="-", alpha=0.6)
                 ax.axvline(x=z, color="k", linestyle="-", alpha=0.6)
-    fig.colorbar(im)
+        # fig.colorbar(im)
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im, cax=cax, orientation="vertical")
 
     return fig
 
@@ -145,13 +183,13 @@ def plot_plddt(
 
     fig_kwargs.update(
         {
-            "figsize": (8 * scale, 5),
+            "figsize": (12 * scale, 5),
             "dpi": 150.0,
         }
     )
     fig = plt.figure(**fig_kwargs)
     ax = fig.add_subplot(1, 1, 1)
-    ax.set_title(rf"Predicted CA-lDDT")
+    ax.set_title(rf"Predicted C$\alpha$-lDDT")
 
     ranking = [(v["plddt"].mean(), k) for k, v in outputs.items()]
     ranking.sort(key=lambda x: x[0], reverse=True)
@@ -171,7 +209,7 @@ def plot_plddt(
             marker=".",
             zorder=2,
         )
-        ax.plot(x, y, "-", label=f"rank_{n} ({model_name})", zorder=1)
+        ax.plot(x, y, "-", label=f"Rank {n} ({model_name})", zorder=1)
 
         # Draw chain breaks
         if asym_id is not None:
@@ -179,10 +217,11 @@ def plot_plddt(
             for i, _, _ in boundaries[1:]:
                 z = i - 0.5
                 ax.axvline(x=z, color="k", linestyle="-.", alpha=0.6)
-    ax.legend()
+    ax.legend(ncols=4, fontsize="smaller")
     ax.set_ylim(0, 100)
     ax.set_xlabel("Positions")
     ax.set_ylabel("plDDT")
+    fig.colorbar(ScalarMappable(norm=Normalize(vmin=0.0, vmax=1.0), cmap=plddt_cmap), ax=ax)
 
     return fig
 
@@ -191,8 +230,13 @@ def plot_msa(
     feature_dict: Dict[str, np.ndarray],
     sort_lines: bool = True,
     dpi: float = 150.0,
+    scale_with_len: bool = False,
 ) -> plt.Figure:
-    seq = feature_dict["msa"][0]
+    scale = 1
+    seqlen = feature_dict["msa"][0]
+    if scale_with_len:
+        scale = max(scale, 1 + seqlen // 200)
+
     if "asym_id" in feature_dict:  # Multimer
         ls = [0]
         k = feature_dict["asym_id"][0]
@@ -203,7 +247,7 @@ def plot_msa(
                 ls.append(1)
             k = i
     else:
-        ls = [len(seq)]
+        ls = [len(seqlen)]
     ln = np.cumsum([0] + ls)
 
     try:
@@ -213,7 +257,7 @@ def plot_msa(
 
     msa = feature_dict["msa"][:n]
     gap = msa != 21
-    qid = msa == seq
+    qid = msa == seqlen
     gapid = np.stack([gap[:, ln[i] : ln[i + 1]].max(-1) for i in range(len(ls))], -1)
     lines = []
     nn = []
@@ -233,7 +277,7 @@ def plot_msa(
 
     nn = np.cumsum(np.append(0, nn))
     lines = np.concatenate(lines, 0)
-    fig = plt.figure(figsize=(8, 5), dpi=dpi)
+    fig = plt.figure(figsize=(12 * scale, 5), dpi=dpi)
     ax = fig.add_subplot(1, 1, 1)
     im = ax.imshow(
         lines,
